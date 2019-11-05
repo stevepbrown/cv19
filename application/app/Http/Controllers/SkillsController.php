63fdfbffaec9b19f96037bded8f0804e9f424eed
Steve Brown <8r0wn5t3r@gmail.com>
2019-11-05 17:07:40 +0000

    Skills controller renders basic view (components ref working in blade)

--- application/app/Http/Controllers/SkillsController.php
+++ application/app/Http/Controllers/SkillsController.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class SkillsController extends Controller
 {
-    public function __invoke()
-    {
+    public function __invoke(){
         return view('skills', 
         ['title' => 'Skills',
         'keywords'=>'IMPORTANT - PLEASE INSERT KEYWORDS',
--- application/app/Providers/AppServiceProvider.php
+++ application/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+           /*        
+        Aliasing Components
+        If your Blade components are stored in a sub-directory, you may wish to alias them for easier access. You may use the component method to alias the component from components.alert to alert. Typically, this should be done in the boot method of your AppServiceProvider:
+        */
+
+
+        Blade::component('laravel-components.component_head', 'head');
+        Blade::component('laravel-components.component_scripts', 'scripts');
     }
 }
