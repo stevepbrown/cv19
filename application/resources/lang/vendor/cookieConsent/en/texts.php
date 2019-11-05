01473a48081d91a69923394ffa0e9d852fa27a2a
Steve Brown <8r0wn5t3r@gmail.com>
2019-11-05 16:40:55 +0000

    Spatie cookie consent added & working

---
+++ application/resources/lang/vendor/cookieConsent/en/texts.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'message' => '<div id="div-cookie-consent">
+    <div id="div-cookie-statment" class="font-display">
+        <p>This site uses session and other cookies to provide a secure & rewarding experience for users. By using the
+          site you are consenting to the use of cookies.</p>
+        <p>For more information on the use of cookies, please consult the <a id=a-show-policy class=hideit href=#>Cookie
+            Policy</a></p>
+      </div>
+    </div>',
+    'agree' => 'OK',
+];
