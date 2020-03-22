fb0cfdff7d4653988216981e3ec9c8f65ac2db5d
stevepbrown <8r0wn5t3r@gmail.com>
2020-03-22 17:52:21 +0000

    Home page renders without working footer - WIP

--- application/routes/api.php
+++ application/routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 // Pingbacks
-Route::middleware('auth:api')->post('/pingback','PingbackController');
+// Route::middleware('auth:api')->post('/pingback','PingbackController');
 
